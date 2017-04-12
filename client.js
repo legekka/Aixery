c3298c256f89746499f801d14def4251e7f14fdd
legekka Yamori <legekka Yamori>
2017-04-13 00:02:53 +0200

    0.0.3

--- client.js
+++ client.js
@@ -1,1 +1,42 @@
 var ws = require('websocket');
+var WebSocketClient = require('websocket').client;
+var client = new WebSocketClient();
+
+function initialize() {
+
+}
+
+
+start();
+
+function start() {
+    client.on('connectFailed', function (error) {
+        console.log('Connect Error: ' + error.toString());
+    });
+
+    client.on('connect', function (connection) {
+        console.log('WebSocket Client Connected');
+
+        var inp = process.openStdin();
+        inp.addListener('data', (d) => {
+            connection.sendUTF(d);
+        });
+
+        connection.on('message', function (message) {
+            if (message.type === 'utf8') {
+                console.log(message.utf8Data);
+            }
+        });
+
+        connection.on('error', function (error) {
+            console.log("Connection Error: " + error.toString());
+        });
+        connection.on('close', function () {
+            console.log('Disconnected from Leibniz.');
+            process.exit();
+        });
+    });
+
+    client.connect('ws://localhost:9669/', 'echo-protocol', JSON.stringify({ "username": "testuser", "key": 'newuser' }));
+}
+
