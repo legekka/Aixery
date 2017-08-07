f6657dc4caf9191e9e7312db8ea29c58e1408dfb
legekka <legekka@gmail.com>
2017-08-07 04:04:38 +0200

    1.3.0 másik neurális modell

--- waifu2xcaffe.bat
+++ waifu2xcaffe.bat
@@ -1,3 +1,3 @@
 L:
 cd L:\Programs\waifu2x-caffe
-waifu2x-caffe-cui.exe -i %1 -o %2 -m noise_scale -s 4 -n 3 --model_dir "L:\Programs\waifu2x-caffe\models\upconv_7_anime_style_art_rgb"
+waifu2x-caffe-cui.exe -i %1 -o %2 -m noise_scale -s 4 -n 3 --model_dir "L:\Programs\waifu2x-caffe\models\anime_style_art_rgb"
