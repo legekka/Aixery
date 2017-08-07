964a38afcd76b6355b13cafc1cd640a66849746f
legekka <legekka@gmail.com>
2017-08-07 03:39:51 +0200

    1.2.0 apikey removed

--- module/upload.js
+++ module/upload.js
@@ -2,6 +2,8 @@ var request = require('request');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
+var api_key = fs.readFileSync('../apikey.txt').toString().trim();
+
 module.exports = {
     upload: (path, callback) => {
         if (fs.existsSync(path)) {
@@ -13,7 +15,7 @@ module.exports = {
                     "FileFormName": "file",
                     "wizard": "true",
                     "file": fs.createReadStream(path),
-                    "key": "H5nCfdM1ddvOfGNj9y3NDw77ojsQf2eGUZWUimsdbXCgCy13GD1zi3wIqEYS",
+                    "key": api_key,
                     "gen": "3.2",
                     "ResponseType": "Text",
                     "RegexList": [
@@ -24,7 +26,7 @@ module.exports = {
                     ],
                     "URL": "$1,1$$2,1$/$3,1$$4,1$",
                     "ThumbnailURL": "",
-                    "DeletionURL": "https://s-ul.eu/delete.php?key=H5nCfdM1ddvOfGNj9y3NDw77ojsQf2eGUZWUimsdbXCgCy13GD1zi3wIqEYS&file=$3,1$"
+                    "DeletionURL": "https://s-ul.eu/delete.php?key=" + api_key + "&file=$3,1$"
                 }
             }, function (err, resp, body) {
                 if (err) {
